65b40003a4de98e479842a448f512e1d1a663564
tkv <timo.vesalainen@iki.fi>
2013-10-20 14:45:31 +0200

    Some helper methods

--- src/org/vesalainen/gpx/GPX.java
+++ src/org/vesalainen/gpx/GPX.java
@@ -5,6 +5,7 @@
 
 package org.vesalainen.gpx;
 
+import com.topografix.gpx._1._1.ExtensionsType;
 import com.topografix.gpx._1._1.GpxType;
 import com.topografix.gpx._1._1.ObjectFactory;
 import com.topografix.gpx._1._1.TrkType;
@@ -23,6 +24,9 @@ import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * @author Timo Vesalainen
@@ -97,6 +101,108 @@ public class GPX
             throw new IOException(ex);
         }
     }
+    public static String getText(ExtensionsType extensions, String namespaceURI, String localname)
+    {
+        for (Object ob : extensions.getAny())
+        {
+            if (ob instanceof Element)
+            {
+                Element el = (Element) ob;
+                if (
+                        namespaceURI.equals(el.getNamespaceURI()) &&
+                        localname.equals(el.getLocalName())
+                        )
+                {
+                    return el.getTextContent();
+                }
+            }
+        }
+        return null;
+    }
+    public static double departure(WptType loc1, WptType loc2)
+    {
+        return Math.cos(Math.toRadians((loc2.getLat().doubleValue()+loc1.getLat().doubleValue())/2));
+    }
+    /**
+     * Return bearing from wp1 to wp2 in degrees
+     * @param wp1
+     * @param wp2
+     * @return 
+     */
+    public static double bearing(WptType wp1, WptType wp2)
+    {
+        double dep = departure(wp1, wp2);
+        double aa = dep*(wp2.getLon().doubleValue()-wp1.getLon().doubleValue());
+        double bb = wp2.getLat().doubleValue()-wp1.getLat().doubleValue();
+        double dd = Math.atan2(aa, bb);
+        if (dd < 0)
+        {
+            dd += 2*Math.PI;
+        }
+        return Math.toDegrees(dd);
+    }
+    /**
+     * Return distance between wp1 and wp2 in NM
+     * @param wp1
+     * @param wp2
+     * @return 
+     */
+    public static double distance(WptType wp1, WptType wp2)
+    {
+        double dep = departure(wp1, wp2);
+        return 60*Math.hypot(
+                wp1.getLat().doubleValue()-wp2.getLat().doubleValue(),
+                dep*(wp1.getLon().doubleValue()-wp2.getLon().doubleValue())
+                );
+    }
+    public abstract static class WaypointFilter
+    {
+        private double bearingTolerance;
+        private double minDistance;
+        private double lastBearing = Double.NaN;
+        private WptType last;
+
+        public WaypointFilter(double bearingTolerance, double minDistance)
+        {
+            this.bearingTolerance = bearingTolerance;
+            this.minDistance = minDistance;
+        }
+
+        public void input(WptType wp)
+        {
+            if (last == null)
+            {
+                last = wp;
+                output(wp);
+            }
+            else
+            {
+                if (Double.isNaN(lastBearing))
+                {
+                    lastBearing = bearing(last, wp);
+                    output(wp);
+                }
+                else
+                {
+                    double bearing = bearing(last, wp);
+                    double distance = distance(last, wp);
+                    if (
+                            Math.abs(bearing-lastBearing) > bearingTolerance &&
+                            distance > minDistance
+                            )
+                    {
+                        last = wp;
+                        lastBearing = bearing;
+                        output(wp);
+                    }
+                }
+            }
+        }
+
+        protected abstract void output(WptType wp);
+        
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -104,20 +210,31 @@ public class GPX
     {
         try
         {
-            File f = new File("trackit.gpx");
+            File f = new File("C:\\Users\\tkv\\Dropbox\\Brest - La Coruna.gpx");
             GPX gpx = new GPX(f);
             JAXBElement<GpxType> gpx1 = gpx.getGpx();
             GpxType gpxType = gpx1.getValue();
             List<TrkType> trkList = gpxType.getTrk();
             for (TrkType trk : trkList)
             {
+                ExtensionsType extensions = trk.getExtensions();
+                System.err.println(getText(extensions, "http://www.opencpn.org", "guid"));
                 List<TrksegType> trksegList = trk.getTrkseg();
                 for (TrksegType tt : trksegList)
                 {
+                    WaypointFilter filter = new WaypointFilter(1.0, 0.1)
+                    {
+                        @Override
+                        protected void output(WptType wt)
+                        {
+                            System.err.println(wt.getLat()+" "+wt.getLon()+" "+wt.getTime());
+                        }
+                    };
+
                     List<WptType> trkptList = tt.getTrkpt();
                     for (WptType wt : trkptList)
                     {
-                        System.err.println(wt.getLat()+" "+wt.getLon()+" "+wt.getTime());
+                        filter.input(wt);
                     }
                 }
             }
